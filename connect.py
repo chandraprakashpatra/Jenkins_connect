2c9bc041e74c888c6ba6f1f6443ff36a7e29b52c
chandraprakashpatra <chandraprakashpatra2002@gmail.com>
2024-09-08 15:16:22 +0530

    jenkins will automatically poll and run if detect any change.

--- connect.py
+++ connect.py
@@ -1,4 +1,6 @@
 
+print("CHECKING POLL SCM : which should automatically check whether there is any new push ! if there is any new push it will run else it will no run !")
+
 print("3rd commit here !")
 
 print("This is a python file. and this is pushed through DEV > GIT > JENKINS > VM")
