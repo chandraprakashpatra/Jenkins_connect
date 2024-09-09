5dfa1698c5432e076c5d6117f6d044874f8b8716
chandraprakashpatra <chandraprakashpatra2002@gmail.com>
2024-09-09 09:45:45 +0530

    automatically run when push

--- connect.py
+++ connect.py
@@ -1,4 +1,6 @@
 
+print("automatically update on vm when changed.")
+
 print("CHECKING POLL SCM : which should automatically check whether there is any new push ! if there is any new push it will run else it will no run !")
 
 print("3rd commit here !")
